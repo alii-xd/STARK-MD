9756e7cb081f7e1e067199976e9af78fdfec6038
alii-xd <alirazagaming94@gmail.com>
2025-04-29 23:40:24 +0500

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -1,39 +1,51 @@
-let handler = async(m, { conn, text, usedPrefix, command }) => {
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  // Sound
+  let name = m.pushName || conn.getName(m.sender)
+  var vn = 'https://cdn.jsdelivr.net/gh/Guru322/GURU-BOT@main/Assets/mp3/Alive.mp3'
+  let url = 'https://github.com/Guru322/GURU-BOT'
+  let murl = 'https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i'
+  let img = 'https://cdn.wallpapersafari.com/71/19/7ZfcpT.png'
+  let con = {
+    key: {
+      fromMe: false,
+      participant: `${m.sender.split`@`[0]}@s.whatsapp.net`,
+      ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}),
+    },
+    message: {
+      contactMessage: {
+        displayName: `${name}`,
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+      },
+    },
+  }
+  let doc = {
+    audio: {
+      url: vn,
+    },
+    mimetype: 'audio/mpeg',
+    ptt: true,
+    waveform: [100, 0, 100, 0, 100, 0, 100],
+    fileName: 'Guru',
 
-    // Sound
-    let name = m.pushName || conn.getName(m.sender)
-    var vn = "https://github.com/PRINCE-GDS/PRINXE-MD/raw/main/Assets/mp3/Audio5.mp3"
-    let url = "https://github.com/PRINCE-GDS/PRINXE-MD"
-    let murl = "https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i"
-    let img = "https://telegra.ph/file/d62a9e66f4311113a72d5.jpg"
-    let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
-    let doc = {
-        audio: {
-          url: vn
-        },
-        mimetype: 'audio/mp4',
-        ptt: true,
-        waveform:  [100, 0, 100, 0, 100, 0, 100],
-        fileName: "Guru",
-    
-        contextInfo: {
-          mentionedJid: [m.sender],
-          externalAdReply: {
-          title: "🎗️ᴘʀɪɴᴄᴇ ᴍᴅ ɪs ᴀʟɪᴠᴇ ʀᴇᴄɪᴛᴇ ᴅᴀʀᴏᴏᴅ sʜᴀʀᴇᴇғ🎗️",
-          body: "PRINCE BOT",
-          thumbnailUrl: img,
-          sourceUrl: 'https://chat.whatsapp.com/Jo5bmHMAlZpEIp75mKbwxP',
-          mediaType: 1,
-          renderLargerThumbnail: true
-          }}
-      };
-    
-      await conn.sendMessage(m.chat, doc, { quoted: con });
-    
-    }
-    
-    handler.help = ['alive']
-    handler.tags = ['main']
-    handler.command = /^(alive)$/i 
+    contextInfo: {
+      mentionedJid: [m.sender],
+      externalAdReply: {
+        title: 'I AM ALIVE',
+        body: '𝐒ʈαɣƙ 𝐌Ɗ',
+        thumbnailUrl: img,
+        sourceUrl: 'https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp',
+        mediaType: 1,
+        renderLargerThumbnail: true,
+      },
+    },
+  }
 
-    export default handler;
+  await conn.sendMessage(m.chat, doc, { quoted: con })
+}
+
+handler.help = ['alive']
+handler.tags = ['main']
+handler.command = /^(alive)$/i
+handler.desc = 'Check if the bot is alive'
+
+export default handler
