2f769c829c74cb330de369877cda72e9038f75b9
alii-xd <alirazagaming94@gmail.com>
2025-05-01 02:34:41 +0500

    Update gp-resetwarn.js

--- plugins/gp-resetwarn.js
+++ plugins/gp-resetwarn.js
@@ -2,23 +2,23 @@ let handler = async (m, { conn, args, groupMetadata }) => {
   let who
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
   else who = m.chat
-  if (!who) throw `✳️ Tag or mention someone`
+  if (!who) throw `*✳️ Tag or mention users*`
   if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
   let warn = global.db.data.users[who].warn
   if (warn > 0) {
     global.db.data.users[who].warn -= 1
-    m.reply(`⚠️ *DELWARN*
+    m.reply(`*⌈⚠️ DELWARN ⌋*
          
-▢ Warns: *-1*
-▢ Warns total: *${warn - 1}*`)
+*▢ WARNS: -1*
+*▢ WARNS TOTAL:* *${warn - 1}*`)
 //    m.reply(`✳️ An admin lowered their warning, now you have *${warn - 1}*`, who)
   } else if (warn == 0) {
-    m.reply('✳️ The user has no warning')
+    m.reply('*✳️ The user has no warning*')
   }
 }
 handler.help = ['delwarn @user']
 handler.tags = ['group']
-handler.command = ['delwarn', 'unwarn']
+handler.command = ['delwarn', 'resetwarn']
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
