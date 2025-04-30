64c4dcd870dcfa0b5a7458721acda88a5cb68520
alii-xd <alirazagaming94@gmail.com>
2025-05-01 01:05:59 +0500

    Update dl-ytv.js

--- plugins/dl-ytv.js
+++ plugins/dl-ytv.js
@@ -1,22 +1,32 @@
-let handler = async(m, { conn, command }) => {
-  try {
-  let who
-  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-  else who = m.quoted.sender ? m.quoted.sender : m.sender
-  let name = await conn.getName(who)
-  m.reply(name)
-  } catch {
-    try {
-    let who = m.quoted ? m.quoted.sender : m.sender
-    let name = await conn.getName(who)
-    m.reply(name)
-  } catch {
-    throw `sorry cant try anything else⍨`
-    }
-  }
-}
-handler.help = ['getname <@tag/reply>']
-handler.tags = ['fun']
-handler.command = /^(get)?name?a?$/i
+/* 
+- Downloader Ytmp4 By Izumi-kzx
+- Power By Team Code Titans
+- https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y 
+*/
+// *[ 🍟 YTMP4 DOWNLOADER ]*
+import fetch from 'node-fetch'
 
-module.exports = handler
+let handler = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, '• YouTube link is required.', m)
+
+try {
+let apiUrl = `https://api.diioffc.web.id/api/download/ytmp4?url=${encodeURIComponent(text)}`
+let response = await fetch(apiUrl)
+let result = await response.json()
+
+if (!result.status) throw new Error('No search video.')
+
+let { title, thumbnail, views, duration, download } = result.result
+let info = `• *Title:* ${title}\n• *Views:* ${views.toLocaleString()}\n• *Duration:* ${duration.timestamp}`
+
+await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: info }, { quoted: m })
+await conn.sendMessage(m.chat, { video: { url: download.url }, caption: title }, { quoted: m })
+
+} catch (error) {
+console.error(error)
+conn.reply(m.chat, '❌ Error downloading your video.', m)
+}}
+
+handler.command = ['ytmp4']
+
+export default handler
