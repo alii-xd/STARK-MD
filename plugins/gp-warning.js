cda182a4a257c26fb37384b07a7ffe69528dea8d
alii-xd <alirazagaming94@gmail.com>
2025-05-01 10:38:38 +0500

    Update gp-warning.js

--- plugins/gp-warning.js
+++ plugins/gp-warning.js
@@ -1,39 +1,48 @@
 let war = global.maxwarn
-let handler = async (m, { conn, text, args, groupMetadata, usedPrefix, command }) => {
+let handler = async (m, { oreo, text, args, groupMetadata, usedPrefix, command }) => {
   let who
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
   else who = m.chat
   if (!who) throw `✳️ Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @user`
   if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-  let name = conn.getName(m.sender)
+  let name = oreo.getName(m.sender)
   let warn = global.db.data.users[who].warn
   if (warn < war) {
     global.db.data.users[who].warn += 1
     m.reply(
       `
-*⌈⚠️ WARNING ⌋*
-*╭───────────────┄┈*
-*├▢ Admin:* ${name}
-*├▢ User:* @${who.split`@`[0]}
-*├▢ Warns:* ${warn + 1}/${war}
-*├▢ Reason:* ${text}`,
-        null,
+⚠️ *Warned User* ⚠️
+
+▢ *Admin:* ${name}
+▢ *User:* @${who.split`@`[0]}
+▢ *Warns:* ${warn + 1}/${war}
+▢ *Reason:* ${text}`,
+      null,
       { mentions: [who] }
     )
+    m.reply(
+      `
+⚠️ *caution* ⚠️
+You received a warning from an admin
 
+▢ *Warns:* ${warn + 1}/${war} 
+if you receive *${war}* warnings you will be automatically removed from the group`,
+      who
+    )
   } else if (warn == war) {
     global.db.data.users[who].warn = 0
     m.reply(`⛔ The user exceeded the *${war}* warnings will therefore be removed`)
     await time(3000)
-    await conn.groupParticipantsUpdate(m.chat, [who], 'remove')
-
-    
+    await oreo.groupParticipantsUpdate(m.chat, [who], 'remove')
+    m.reply(
+      `♻️ You were removed from the group *${groupMetadata.subject}* because you have been warned *${war}* times`,
+      who
+    )
   }
 }
 handler.help = ['warn @user']
 handler.tags = ['group']
 handler.command = ['warn']
-handler.desc = 'Warn a user'
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
@@ -43,7 +52,3 @@ export default handler
 const time = async ms => {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
-
-
-
-
