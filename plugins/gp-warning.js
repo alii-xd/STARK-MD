69cc413a8353dd03f79c86fe8b1cf1ee5f86692b
alii-xd <alirazagaming94@gmail.com>
2025-05-01 11:39:41 +0500

    Update gp-warning.js

--- plugins/gp-warning.js
+++ plugins/gp-warning.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn, text, args, groupMetadata, usedPrefix, command }
 *├▢ ADMIN:* ${name}
 *├▢ USER:* @${who.split`@`[0]}
 *├▢ WARNS:* ${warn + 1}/${war}
-*├▢ REASON:* 
+*├▢ REASON:* ${text}
 *╰───────────────┄┈*`, 
      null, { mentions: [who] }) 
         
