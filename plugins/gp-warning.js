57b4047ba5ccab6511f1ca6269b17d6ea23a95e7
alii-xd <alirazagaming94@gmail.com>
2025-05-01 16:24:59 +0500

    Update gp-warning.js

--- plugins/gp-warning.js
+++ plugins/gp-warning.js
@@ -1,25 +1,31 @@
-let war = global.maxwarn
-let handler = async (m, { conn, text, args, groupMetadata, usedPrefix, command }) => {
-  let who
-  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-  else who = m.chat
-  if (!who) throw `*✳️ Tag or mention someone*\n\n*📌 Example : ${usedPrefix + command} @user*`
-  if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-  let name = conn.getName(m.sender)
-  let warn = global.db.data.users[who].warn
-  if (warn < war) {
-    global.db.data.users[who].warn += 1
-    m.reply(
-      `
+let war = global.maxwarn;
+let handler = async (m, { conn, text, args, groupMetadata, usedPrefix, command }) => {      
+    let who;
+    if (m.isGroup) {
+        who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
+    } else {
+        who = m.chat;
+    }
+    if (!who) throw `⚠️ *Tag or mention someone!*\n\n💡 *Example:* ${usedPrefix + command} @user`;
+
+    if (!(who in global.db.data.users)) throw `⚠️ *User not found in the database!*`;
+
+    let name = conn.getName(m.sender);
+    let warn = global.db.data.users[who].warn;
+
+    if (warn < war) {
+        global.db.data.users[who].warn += 1;
+
+        m.reply(`
 *⌈⚠️ WARNING ⌋*
-*╭───────────────┄┈*
-*├▢ ADMIN:* ${name}
-*├▢ USER:* @${who.split`@`[0]}
-*├▢ WARNS:* ${warn + 1}/${war}
-*├▢ REASON:* ${text}
-*╰───────────────┄┈*`, 
-     null, { mentions: [who] }) 
-        
+*╭────────────────┄┈*
+*│👮‍♂️ ADMIN:* ${name}
+*│👤 USER:* @${who.split`@`[0]}
+*│⚠️ WARNS:* ${warn + 1}/${war}
+*│📝 REASON:* ${text || "No reason provided"}
+*╰────────────────┄┈*
+`, null, { mentions: [who] });
+
   } else if (warn == war) {
     global.db.data.users[who].warn = 0
     m.reply(`⛔ The user exceeded the *${war}* warnings will therefore be removed`)
@@ -42,3 +48,4 @@ export default handler
 const time = async ms => {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
+
